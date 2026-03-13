50929358684643016fb4b99d6504dbb075d5bc99
Rül Hölos <60986988+RulHolos@users.noreply.github.com>
2026-03-13 20:10:00 +0100

    Proof of concept for auto updating

--- LunaForge/App.xaml.cs
+++ LunaForge/App.xaml.cs
@@ -2,6 +2,8 @@
 using LunaForge.Models;
 using LunaForge.Services;
 using LunaForge.Views;
+using Serilog;
+using Velopack;
 using Application = System.Windows.Application;
 
 namespace LunaForge;
@@ -12,11 +14,13 @@ public partial class App : Application
 
     public const string AppVersion = "v0.10-alpha";
 
-    protected override void OnStartup(StartupEventArgs e)
+    protected override async void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
 
         CoreLogger.Initialize();
+
+        await CheckForUpdatesAsync();
 
         bool setupDone = EditorConfig.Default.Get<bool>("SetupDone").Value;
         if (!setupDone)
@@ -51,5 +55,40 @@ public partial class App : Application
         };
         
         launcher.Show();
+    }
+
+    /// <summary>
+    /// TODO: Move that into some class, like "UpdaterService" + UI.
+    /// </summary>
+    /// <returns></returns>
+    private static async Task CheckForUpdatesAsync()
+    {
+        ILogger logger = CoreLogger.Create("Updater");
+        try
+        {
+            logger.Information("Checking updates...");
+            var mgr = new UpdateManager("https://github.com/LunaForgeDev/LunaForge/releases");
+            if (!mgr.IsInstalled)
+            {
+                logger.Information("Not installed or running dev, skipping update check.");
+                return; // Dev
+            }
+
+            var newVersion = await mgr.CheckForUpdatesAsync();
+            if (newVersion is null)
+            {
+                logger.Information("No update found.");
+                return;
+            }
+
+            // TODO: UI for update confirmation.
+            logger.Information("New update found: {0}", newVersion.TargetFullRelease.Version);
+            await mgr.DownloadUpdatesAsync(newVersion);
+            mgr.ApplyUpdatesAndRestart(newVersion);
+        }
+        catch (Exception ex)
+        {
+            logger.Warning("Impossible to check for updates: {0}", ex);
+        }
     }
 }
